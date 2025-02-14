7e86cb6ce602b6fcdccc14078bb9658247545fe4
shivanshcode <70197059+shivanshcode@users.noreply.github.com>
2025-02-14 14:46:46 +0530

    Update exp_informer.py

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -99,8 +99,6 @@ class Exp_Informer(Exp_Basic):
             shuffle=shuffle_flag,
             num_workers=args.num_workers,
             drop_last=drop_last)
-        print(f'DataLoader: {type(data_loader)}, {type(data_set)},     Location: Exp_informer/_get_data', flush=True)
-
         return data_set, data_loader
 
     def _select_optimizer(self):
